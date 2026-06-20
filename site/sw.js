702c8bed09ce29e1a2d6e3d93500e143980a8676
Ian Buffington <ian.buffington@gmail.com>
2026-06-20 17:37:43 +0000

    Rain: louder background, quieter drops to prevent masking

--- site/sw.js
+++ site/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'noise-v14';
+const CACHE = 'noise-v15';
 const ASSETS = ['/', '/style.css', '/app.js', '/manifest.json', '/icons/icon-192.svg', '/icons/icon-512.svg'];
 
 self.addEventListener('install', (e) => {
